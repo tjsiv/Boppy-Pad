9d60290b9eaa499a1cc8c218f9bbe1ceb4c43cff
tjsiv <tjsivelsjr@gmail.com>
2024-09-08 00:44:20 -0700

    added color picking options and reset functions

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('drawingCanvas');
 const ctx = canvas.getContext('2d');
-
+const penColorInput = document.getElementById('penColor');
+const bgColorInput = document.getElementById('bgColor');
+const resetBtn = document.getElementById('resetBtn');
 
 
 // Set canvas size
@@ -42,5 +44,18 @@ function draw(e) {
 canvas.addEventListener('mousedown', startDrawing);
 canvas.addEventListener('mouseup', stopDrawing);
 canvas.addEventListener('mousemove', draw);
+
+penColorInput.addEventListener('change', (e) => {
+    penColor = e.target.value;
+});
+
+bgColorInput.addEventListener('change', (e) => {
+    bgColor = e.target.value;
+    setCanvasBackground(bgColor);
+});
+
+resetBtn.addEventListener('click', () => {
+    setCanvasBackground(bgColor);
+});
 
 setCanvasBackground(bgColor);
